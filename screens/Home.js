6b04071c76dd06f52ad3c1c6e755dae0b779ed63
Christian Laino <christianlaino@Christians-MacBook-Air.local>
2021-09-09 19:51:55 -0300

    bottom navigation and svg icon component

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,22 @@
-import React, { useEffect } from 'react';
-import {  Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 import Button from '../components/buttons/Button';
 import { useDispatch } from 'react-redux';
 import { login } from '../redux/slices/auth';
 import Container from '../components/Container'
+import BottomNavigator from '../components/BottomNavigator';
+import { Icon } from '../components/icons';
+
+const TABS = (selected) => [
+    {
+        icon: <Icon name='tokens' color={selected === 0 ? 'white' : 'gray'} />,
+        text: 'Tokens'
+    },
+    {
+        icon: <Icon name='nfts' color={selected === 1 ? 'white' : 'gray'} />,
+        text: 'NFTs'
+    }
+]
 
 function Home() {
     const dispatch = useDispatch();
@@ -12,6 +25,13 @@ function Home() {
         dispatch(login(false));
     }
 
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const onSelect = (value) => setSelectedTab(value);
+
+    const tabs = TABS(selectedTab);
+    console.log(tabs);
+
     return (
         <Container>
             <Text>HOME</Text>
@@ -19,6 +39,11 @@ function Home() {
                 text='Logout'
                 onPress={onPress}
                 variant="rainbow"
+            />
+            <BottomNavigator
+                tabs={tabs}
+                selected={selectedTab}
+                onSelect={onSelect}
             />
         </Container>
     );
