b6db223cb2eb4a763f4fbd6ff4aae53f48c3902b
rocky-fleek <tomas@fleek.co>
2021-09-23 14:27:39 -0300

    Added transformer for nodejs std libs

---
+++ transformer.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const babel = require('babel-core');
+
+/**
+ * This is your `.babelrc` equivalent.
+ */
+const babelRC = {
+  presets: [require('babel-preset-react-native')],
+  plugins: [
+    // The following plugin will rewrite imports. Reimplementations of node
+    // libraries such as `assert`, `buffer`, etc. will be picked up
+    // automatically by the React Native packager.  All other built-in node
+    // libraries get rewritten to their browserify counterpart.
+    [
+      require('babel-plugin-rewrite-require'),
+      {
+        aliases: {
+          crypto: 'crypto-browserify',
+          stream: 'stream-browserify',
+          assert: 'assert-browserify',
+          // constants: 'constants-browserify',
+          // dns: 'node-libs-browser/mock/dns',
+          // domain: 'domain-browser',
+          // fs: 'node-libs-browser/mock/empty',
+          // http: 'stream-http',
+          // https: 'https-browserify',
+          // net: 'node-libs-browser/mock/net',
+          // os: 'os-browserify/browser',
+          // path: 'path-browserify',
+          // querystring: 'querystring-es3',
+          // _stream_duplex: 'readable-stream/duplex',
+          // _stream_passthrough: 'readable-stream/passthrough',
+          // _stream_readable: 'readable-stream/readable',
+          // _stream_transform: 'readable-stream/transform',
+          // _stream_writable: 'readable-stream/writable',
+          // sys: 'util',
+          // timers: 'timers-browserify',
+          // tls: 'node-libs-browser/mock/tls',
+          // tty: 'tty-browserify',
+          // vm: 'vm-browserify',
+          // zlib: 'browserify-zlib',
+
+          // You can also mock any libraries that you don't need to support on
+          // React Native, but have to be present for 3rd party code to work:
+        },
+        throwForNonStringLiteral: true,
+      },
+    ],
+  ],
+};
+
+function transform(src, filename, options) {
+  const babelConfig = Object.assign({}, babelRC, {
+    filename,
+    sourceFileName: filename,
+  });
+  const result = babel.transform(src, babelConfig);
+  return {
+    ast: result.ast,
+    code: result.code,
+    map: result.map,
+    filename: filename,
+  };
+}
+
+module.exports = function (data, callback) {
+  let result;
+  try {
+    result = transform(data.sourceCode, data.filename, data.options);
+  } catch (e) {
+    callback(e);
+    return;
+  }
+  callback(null, result);
+};
