dc501bd66bf7ee02718d1f7d22bda9484432b5e2
Christian Laino <christianmlaino@gmail.com>
2021-10-27 14:05:51 -0300

    Plug mobile deposit screen sc 24231 (#23)
    * deposit
    
    * copy to clipboard
    
    Co-authored-by: Christian Laino <christianlaino@Christians-MacBook-Air.local>

---
+++ src/components/common/InfoWithActions/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import animationScales from '../../../utils/animationScales';
+import Touchable from '../../animations/Touchable';
+import Icon from '../../icons';
+import styles from './styles';
+
+const InfoWithActions = ({ text, colors, actions }) => {
+  return (
+    <LinearGradient colors={colors} style={styles.gradient}
+      start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
+
+      <View style={styles.container}>
+
+        <Text style={styles.text}>{text}</Text>
+
+        <View style={styles.actionsContainer}>
+          {actions.map(action => (
+            <View style={styles.action}>
+              <Touchable onPress={action.onPress} scale={animationScales.large}>
+                <Icon name={action.icon} color='white' />
+              </Touchable>
+            </View>
+          ))}
+        </View>
+
+      </View>
+    </LinearGradient>
+  );
+}
+
+export default InfoWithActions;
---
+++ src/screens/Deposit/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import { FontStyles } from "../../constants/theme";
+
+export default StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    marginBottom: 40,
+  },
+  title: {
+    ...FontStyles.Subtitle3,
+  },
+  text: {
+    ...FontStyles.NormalGray,
+    marginVertical: 20,
+  },
+  divider: {
+    width: '42%',
+    justifyContent: 'center',
+  }
+})
