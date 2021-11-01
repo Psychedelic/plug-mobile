940ca937404b4c119a72be82097881f1b8b14bd4
Christian Laino <christianlaino@Christians-MacBook-Air.local>
2021-11-01 15:27:33 -0300

    finished contacts

--- src/components/common/InfoWithActions/index.js
+++ src/components/common/InfoWithActions/index.js
@@ -8,26 +8,26 @@ import styles from './styles';
 
 const InfoWithActions = ({ text, colors, actions }) => {
   return (
-    <LinearGradient colors={colors} style={styles.gradient}
-      start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
-
+    <LinearGradient
+      colors={colors}
+      style={styles.gradient}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}>
       <View style={styles.container}>
-
         <Text style={styles.text}>{text}</Text>
 
         <View style={styles.actionsContainer}>
           {actions.map(action => (
             <View style={styles.action}>
               <Touchable onPress={action.onPress} scale={animationScales.large}>
-                <Icon name={action.icon} color='white' />
+                <Icon name={action.icon} color="white" />
               </Touchable>
             </View>
           ))}
         </View>
-
       </View>
     </LinearGradient>
   );
-}
+};
 
 export default InfoWithActions;
