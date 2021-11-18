5f2ed281eff9a815bc210e471c6a594a84929244
Christian Laino <christianlaino@Christians-MacBook-Air.local>
2021-11-18 15:18:28 -0300

    copy principal in wallet detail

--- src/components/common/AccountInfo/index.js
+++ src/components/common/AccountInfo/index.js
@@ -3,15 +3,25 @@ import { FontStyles } from '../../../constants/theme';
 import { StyleSheet, View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import shortAddress from '../../../helpers/short-address';
+import Clipboard from '@react-native-community/clipboard';
+import Touchable from '../../animations/Touchable';
 
 const AccountInfo = () => {
   const { currentWallet } = useSelector(state => state.keyring);
   const { principal, name } = currentWallet || {};
+
+  const copyToClipboard = async () => {
+    Clipboard.setString(principal);
+    console.log(await Clipboard.getString())
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={FontStyles.Normal}>{name}</Text>
-      <Text style={FontStyles.SmallGray}>{shortAddress(principal)}</Text>
-    </View>
+    <Touchable onPress={copyToClipboard}>
+      <View style={styles.container}>
+        <Text style={FontStyles.Normal}>{name}</Text>
+        <Text style={FontStyles.SmallGray}>{shortAddress(principal)}</Text>
+      </View>
+    </Touchable>
   );
 };
 
