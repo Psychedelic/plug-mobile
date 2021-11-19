e6b806194ef6bcb124ef6ee1ec3889d73daa1979
rocky-fleek <tomas@fleek.co>
2021-11-19 16:05:47 -0300

    Compiles on my version

--- metro.config.js
+++ metro.config.js
@@ -12,6 +12,28 @@ module.exports = {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
+      minifierConfig: {
+        keep_classnames: true, // FIX typeorm
+        keep_fnames: true, // FIX typeorm
+        mangle: {
+          // toplevel: false,
+          keep_classnames: true, // FIX typeorm
+          keep_fnames: true, // FIX typeorm
+        },
+        output: {
+          ascii_only: true,
+          quote_style: 3,
+          wrap_iife: true,
+        },
+        sourceMap: {
+          includeSources: false,
+        },
+        toplevel: false,
+        compress: {
+          // reduce_funcs inlines single-use functions, which cause perf regressions.
+          reduce_funcs: false,
+        },
+      },
     }),
   },
   resolver: {
