4fc0cc750b3980146e0309cbd03fd6c52d1323b7
Christian Laino <christianlaino@Christians-MacBook-Air.local>
2021-11-30 17:36:58 -0300

    fixed svgs

--- src/components/icons/svg/LightingActivityIcon.js
+++ src/components/icons/svg/LightingActivityIcon.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Svg, { G, Rect, Path, Defs, LinearGradient, Stop, } from 'react-native-svg';
 
-const LightingActivityIcon = ({ color = 'black', ...props }) => (
-  <Svg width="51" height="51" viewBox="0 0 51 51" {...props}>
-    <G filter="url(#filter0_ddd)">
-      <Rect x="16" y="14" width="19" height="19" rx="9.5" fill="white" />
-      <Rect x="16.6" y="14.6" width="17.8" height="17.8" rx="8.9" stroke="url(#paint0_linear)" strokeWidth="1.2" />
-    </G>
-    <Path d="M28.5566 24.0644L24.0742 27.9844C23.9512 28.0898 23.8106 28.125 23.6875 28.125C23.5645 28.125 23.4414 28.0898 23.3535 28.0195C23.1426 27.8789 23.0547 27.5977 23.1602 27.3516L24.5137 24.1875H22.5625C22.3164 24.1875 22.1055 24.0469 22.0352 23.8359C21.9473 23.6074 22 23.3613 22.1758 23.2031L26.6758 19.2832C26.8691 19.1074 27.1504 19.1074 27.3613 19.2656C27.5898 19.4062 27.6602 19.6875 27.5547 19.9336L26.2012 23.0801H28.1699C28.3984 23.0801 28.6094 23.2383 28.6973 23.4492C28.7852 23.6602 28.7324 23.9062 28.5566 24.0644Z" fill="url(#paint1_linear)" />
+const LightingActivityIcon = ({ color, ...props }) => (
+  <Svg width="19" height="19" viewBox="0 0 19 19" {...props}>
+    <Rect x="0.6" y="0.6" width="17.8" height="17.8" rx="8.9" fill="white" stroke="url(#paint0_linear_4235_1860)" stroke-width="1.2" />
+    <Path d="M12.5566 10.0644L8.07422 13.9844C7.95118 14.0898 7.81055 14.125 7.6875 14.125C7.56446 14.125 7.44141 14.0898 7.35352 14.0195C7.14258 13.8789 7.05469 13.5977 7.16016 13.3516L8.51368 10.1875H6.5625C6.31641 10.1875 6.10547 10.0469 6.03516 9.83593C5.94727 9.60742 6 9.36132 6.17578 9.20312L10.6758 5.2832C10.8691 5.10742 11.1504 5.10742 11.3613 5.26562C11.5898 5.40625 11.6602 5.6875 11.5547 5.93359L10.2012 9.08007H12.1699C12.3984 9.08007 12.6094 9.23828 12.6973 9.44921C12.7852 9.66015 12.7324 9.90625 12.5566 10.0644Z" fill="url(#paint1_linear_4235_1860)" />
     <Defs>
-
-      <LinearGradient id="paint0_linear" x1="17.5" y1="16.5" x2="33" y2="30.5" gradientUnits="userSpaceOnUse">
+      <LinearGradient id="paint0_linear_4235_1860" x1="1.5" y1="2.5" x2="17" y2="16.5" gradientUnits="userSpaceOnUse">
         <Stop stopColor="#FFEF5E" />
         <Stop offset="1" stopColor="#FF9518" />
       </LinearGradient>
-      <LinearGradient id="paint1_linear" x1="22.5" y1="20" x2="29" y2="28" gradientUnits="userSpaceOnUse">
+      <LinearGradient id="paint1_linear_4235_1860" x1="6.5" y1="6" x2="13" y2="14" gradientUnits="userSpaceOnUse">
         <Stop stopColor="#FFEA5A" />
         <Stop offset="1" stopColor="#FF9A1C" />
       </LinearGradient>
--- src/components/icons/svg/MintActivityIcon.js
+++ src/components/icons/svg/MintActivityIcon.js
@@ -2,24 +2,21 @@ import React from 'react';
 import Svg, { G, Rect, Path, Defs, LinearGradient, Stop } from 'react-native-svg';
 
 const MintActivityIcon = ({ color = '#6B7280', ...props }) => (
-  <Svg width="51" height="51" viewBox="0 0 51 51" {...props}>
-    <G filter="url(#filter0_ddd)">
-      <Rect x="16" y="14" width="19" height="19" rx="9.5" fill="white" />
-      <Rect x="16.6" y="14.6" width="17.8" height="17.8" rx="8.9" stroke="url(#paint0_linear)" stroke-width="1.2" />
-    </G>
-    <Path d="M22.125 20.8125H21C21 22.9746 22.7578 24.75 24.9375 24.75V27C24.9375 27.3164 25.1836 27.5625 25.4824 27.5625C25.7637 27.5625 26.0625 27.3164 26.0625 27V24.75C26.0625 22.5879 24.2871 20.8125 22.125 20.8125ZM28.875 19.6875C27.3809 19.6875 26.0977 20.5137 25.4297 21.7266C25.9043 22.2539 26.2734 22.9043 26.4668 23.6074C28.4531 23.4141 30 21.7266 30 19.6875H28.875Z" fill="url(#paint1_linear)" />
+  <Svg width="19" height="19" viewBox="0 0 19 19" {...props}>
+    <Rect x="0.6" y="0.6" width="17.8" height="17.8" rx="8.9" fill="white" stroke="url(#paint0_linear_4235_1845)" stroke-width="1.2" />
+    <Path d="M6.125 6.8125H5C5 8.97461 6.75781 10.75 8.9375 10.75V13C8.9375 13.3164 9.18359 13.5625 9.48242 13.5625C9.76367 13.5625 10.0625 13.3164 10.0625 13V10.75C10.0625 8.58789 8.28711 6.8125 6.125 6.8125ZM12.875 5.6875C11.3809 5.6875 10.0977 6.51367 9.42969 7.72656C9.9043 8.25391 10.2734 8.9043 10.4668 9.60742C12.4531 9.41406 14 7.72656 14 5.6875H12.875Z" fill="url(#paint1_linear_4235_1845)" />
     <Defs>
-     
-      <LinearGradient id="paint0_linear" x1="17.1585" y1="17.0122" x2="31.2927" y2="31.378" gradientUnits="userSpaceOnUse">
+      <LinearGradient id="paint0_linear_4235_1845" x1="1.15854" y1="3.0122" x2="15.2927" y2="17.378" gradientUnits="userSpaceOnUse">
         <Stop stopColor="#00E8FF" />
         <Stop offset="1" stopColor="#47F648" />
       </LinearGradient>
-      <LinearGradient id="paint1_linear" x1="21.9474" y1="19.9474" x2="27.3947" y2="26.1053" gradientUnits="userSpaceOnUse">
+      <LinearGradient id="paint1_linear_4235_1845" x1="5.94737" y1="5.94737" x2="11.3947" y2="12.1053" gradientUnits="userSpaceOnUse">
         <Stop stopColor="#05E9F5" />
         <Stop offset="1" stopColor="#46F64C" />
       </LinearGradient>
     </Defs>
   </Svg>
+
 )
 
 export default MintActivityIcon;
