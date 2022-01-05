bdfa4d849b05276cb59185d8d1b216e3c8a721e5
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-01-05 13:36:36 -0300

    Add stackActions

--- src/navigation/helper.js
+++ src/navigation/helper.js
@@ -1,9 +1,18 @@
-import { createNavigationContainerRef } from '@react-navigation/native';
+import {
+  createNavigationContainerRef,
+  StackActions,
+} from '@react-navigation/native';
 
 export const navigationRef = createNavigationContainerRef();
 
 export function navigate(route, params) {
   if (navigationRef.isReady()) {
     navigationRef.navigate(route, params);
+  }
+}
+
+export function push(...args) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.push(...args));
   }
 }
