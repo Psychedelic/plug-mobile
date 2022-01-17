94d2aae0ece3eb78d1776240f1e70b4f0512dff8
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-01-17 17:52:55 -0300

    WIP

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    '@babel/plugin-transform-exponentiation-operator',
+    // TODO: Delete the following line if is not being used anymore
+    // '@babel/plugin-transform-exponentiation-operator',
+    'transform-exponentiation-operator',
     'react-native-reanimated/plugin',
   ],
 };
