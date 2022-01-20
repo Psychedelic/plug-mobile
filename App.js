dfd305e5210c08f2ea77665ed980e171957eb166
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-01-20 15:34:23 -0300

    New Reactotron Config

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
+import Reactotron from 'reactotron-react-native';
 import SplashScreen from 'react-native-splash-screen';
-import { Provider, useDispatch, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 
 import ErrorBoundary from './src/components/common/ErrorBoundary';
 import { initKeyring } from './src/redux/slices/keyring';
 import { persistor, store } from './src/redux/store';
 import Routes from './src/navigation';
+import './reactotronConfig';
 
 const PersistedApp = () => {
   const { instance } = useSelector(state => state.keyring);
   const dispatch = useDispatch();
+  console.tron.log('TuVieja');
 
   useEffect(() => {
     if (instance) {
@@ -34,8 +37,4 @@ const App = () => (
   </Provider>
 );
 
-if (__DEV__) {
-  import('./reactotronConfig');
-}
-
-export default App;
+export default __DEV__ ? Reactotron.overlay(App) : App;
