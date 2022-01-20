6a72c2eedb9a3bdd1c47ce64616ef1aab09b98ee
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-01-20 14:40:53 -0300

    Finish animation, extract styles

---
+++ src/components/common/AccountInfo/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+import { Colors } from '../../../constants/theme';
+
+export default StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animationContainer: {
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    height: 25,
+    backgroundColor: Colors.White.Secondary,
+    position: 'absolute',
+    top: 48,
+  },
+  pointer: {
+    height: 10,
+    width: 10,
+    backgroundColor: Colors.White.Secondary,
+    transform: [{ rotate: '45deg' }],
+    position: 'absolute',
+    top: -4,
+  },
+  text: {
+    color: Colors.White.Primary,
+  },
+});
