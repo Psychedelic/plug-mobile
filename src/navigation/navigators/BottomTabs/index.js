6beff8742792f96f2ae7e155820a678ef9e11c70
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-02-11 15:08:45 -0300

    Mix current Design with V2 navigation implementation

--- src/navigation/navigators/BottomTabs/index.js
+++ src/navigation/navigators/BottomTabs/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
 
 import Touchable from '../../../components/animations/Touchable';
-import UserIcon from '../../../components/common/UserIcon';
 import { Colors } from '../../../constants/theme';
 import Icon from '../../../components/icons';
 import styles from './styles';
 
 const BottomTabs = ({ state, navigation }) => {
-  const { currentWallet } = useSelector(store => store.keyring);
+  const tabs = state.routes?.slice(1);
+
   return (
     <View style={styles.root}>
-      {state.routes.map((route, index) => {
-        const isProfile = index === 0;
-        const isTokens = index === 1;
+      {tabs.map((route, index) => {
+        const isTokens = index === 0;
         const label = route.name;
-        const isFocused = state.index === index;
+        const isFocused = state.index === index + 1;
 
         const onPress = () => {
           const event = navigation.emit({
@@ -30,11 +28,7 @@ const BottomTabs = ({ state, navigation }) => {
           }
         };
 
-        return isProfile ? (
-          <View key={index} style={[styles.tab, styles.profileTab]}>
-            <UserIcon icon={currentWallet?.icon} onPress={onPress} />
-          </View>
-        ) : (
+        return (
           <Touchable onPress={onPress} key={route.name}>
             <View key={index} style={styles.tab}>
               <Text>
