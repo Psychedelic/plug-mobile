4b00c373509ffde1423dc03484ce7e3d21021b2f
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-02-11 09:27:52 -0300

    Improve Navigation Stack

--- src/navigation/navigators/SwipeNavigator.js
+++ src/navigation/navigators/SwipeNavigator.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Routes from '../Routes';
+
 import ProfileScreen from '../../screens/Profile';
 import WalletNavigator from './WalletNavigator';
+import Routes from '../Routes';
 
 const Swipe = createMaterialTopTabNavigator();
 
 const renderTabBar = () => null;
 
 const SwipeNavigator = () => (
-  <View style={{ flex: 1 }}>
-    <Swipe.Navigator
-      screenOptions={{
-        tabBarBounces: false,
-      }}
-      initialRouteName={Routes.WALLET_SCREEN}
-      tabBar={renderTabBar}>
-      <Swipe.Screen component={ProfileScreen} name={Routes.PROFILE_SCREEN} />
-      <Swipe.Screen component={WalletNavigator} name={Routes.WALLET_SCREEN} />
-    </Swipe.Navigator>
-  </View>
+  <Swipe.Navigator
+    screenOptions={{
+      tabBarBounces: false,
+    }}
+    initialRouteName={Routes.WALLET_SCREEN}
+    tabBar={renderTabBar}>
+    <Swipe.Screen component={ProfileScreen} name={Routes.PROFILE_SCREEN} />
+    <Swipe.Screen component={WalletNavigator} name={Routes.WALLET_SCREEN} />
+  </Swipe.Navigator>
 );
 
 export default SwipeNavigator;
--- src/navigation/navigators/WalletNavigator.js
+++ src/navigation/navigators/WalletNavigator.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Routes from '../Routes';
+
 import Tokens from '../../screens/Wallet/tabs/Tokens';
 import NFTs from '../../screens/Wallet/tabs/NFTs';
 import BottomTabs from './BottomTabs';
+import Routes from '../Routes';
 
 const Tab = createBottomTabNavigator();
 
 const WalletNavigator = () => (
-  <View style={{ flex: 1 }}>
-    <Tab.Navigator
-      initialRouteName={Routes.TOKENS}
-      screenOptions={{ headerShown: false }}
-      tabBar={props => <BottomTabs {...props} />}>
-      <Tab.Screen component={Tokens} name={Routes.TOKENS} />
-      <Tab.Screen component={NFTs} name={Routes.NFTS} />
-    </Tab.Navigator>
-  </View>
+  <Tab.Navigator
+    initialRouteName={Routes.TOKENS}
+    screenOptions={{ headerShown: false }}
+    tabBar={props => <BottomTabs {...props} />}>
+    <Tab.Screen component={Tokens} name={Routes.TOKENS} />
+    <Tab.Screen component={NFTs} name={Routes.NFTS} />
+  </Tab.Navigator>
 );
 
 export default WalletNavigator;
