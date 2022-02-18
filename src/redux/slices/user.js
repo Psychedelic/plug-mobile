b357519c50841d8554841c6360d1e54dd4a48d1e
rocky-fleek <tomas@fleek.co>
2022-02-18 10:56:14 -0300

    Improved login load speed

--- src/redux/slices/user.js
+++ src/redux/slices/user.js
@@ -118,6 +118,7 @@ export const privateGetAssets = async (params, state) => {
     const response = await instance?.getState();
     const { wallets, currentWalletId } = response || {};
     let assets = wallets?.[currentWalletId]?.assets || [];
+    console.log('private get assets', assets);
     if (
       !assets.length ||
       assets?.every(asset => parseFloat(asset.amount) <= 0) ||
@@ -127,6 +128,7 @@ export const privateGetAssets = async (params, state) => {
     } else {
       instance?.getBalance();
     }
+    console.log('should return pretty quick');
     return { assets, icpPrice };
   } catch (e) {
     console.log('getAssets', e);
