e193993794cfe4e2f0d90fa9289b502d836f3268
Gilbert Jolly <gilbert@jollytechnologies.co.uk>
2022-02-25 17:15:46 +0100

    Call the bridge

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { View, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -11,9 +11,14 @@ import { FontStyles } from '../../constants/theme';
 import Routes from '../../navigation/Routes';
 import styles from './styles';
 
+import { NativeModules } from 'react-native';
+
+const { SigningBridge } = NativeModules;
+
 function Welcome() {
   const navigation = useNavigation();
   const { isInitialized } = useSelector(state => state.keyring);
+  const [signedData, setSignedData] = useState('');
 
   const onPress = flow => () =>
     navigation.navigate(Routes.CREATE_PASSWORD, {
@@ -22,6 +27,20 @@ function Welcome() {
 
   const handleBack = () => {
     navigation.navigate(Routes.LOGIN_SCREEN);
+  };
+
+  const signData = () => {
+    var data = SigningBridge.signData(
+      '{"request_id":[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7],"cert":{"tree":[0],"signature":[],"delegation":null}}',
+    );
+    console.log(`response ${data}`);
+    // setSignedData(data);
+  };
+
+  const demoInterchange = () => {
+    var data = SigningBridge.interchangeDemo('[DEMO_DATA]');
+    console.log(`response ${data}`);
+    setSignedData(data);
   };
 
   return (
@@ -44,6 +63,12 @@ function Welcome() {
           text="Import Wallet"
           onPress={onPress('import')}
         />
+        <Button
+          buttonStyle={[styles.buttonMargin, styles.buttonStyling]}
+          text="Sign Data"
+          onPress={() => demoInterchange()}
+        />
+        <Text style={styles.title}>{signedData}</Text>
       </View>
     </Container>
   );
