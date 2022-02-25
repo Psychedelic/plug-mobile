2d60ade8186cb8ec8fcf234fda3f29ef8bde0bf0
Gilbert Jolly <gilbert@jollytechnologies.co.uk>
2022-02-25 17:15:36 +0100

    iOS Bridge

--- ios/Plug-Bridging-Header.h
+++ ios/Plug-Bridging-Header.h
@@ -2,3 +2,13 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef Plug_Bridging_Header_h
+#define Plug_Bridging_Header_h
+
+#import "libplug_signer.h"
+
+
+
+
+#endif /* Plug_Bridging_Header_h */
+
---
+++ ios/libplug_signer.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+const char* ios_verify_json(const char* to);
+const char* ios_verify_json_example();
+const char* interchange_demo(const char* to);
+
+
