464fc41abb6d9a1b525db56276e087914cf0bbb9
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-03-02 17:19:10 -0300

    Disable font scaling

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 import App from './App';
 // import { name as appName } from './app.json';
 import 'text-encoding-polyfill';
 import './shim';
 
 AppRegistry.registerComponent('Plug', () => App);
+
+// Disable fontScaling.
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
