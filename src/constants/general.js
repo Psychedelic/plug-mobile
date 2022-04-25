949336fde3c7447ef9dc9cd5a96c9fe6f648084d
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-04-25 13:23:15 -0300

    Fix password validation and fix edit contact

--- src/constants/general.js
+++ src/constants/general.js
@@ -1,6 +1,6 @@
 import { ALPHANUM_REGEX } from '@constants/addresses';
 
-export const isValidPassword = password => password && password.length < 12;
+export const isValidPassword = password => password && password.length >= 12;
 
 // This is only going to be used in the CreatePassword screen, we want the users to have a password between 12 and 24 chars:
 export const isValidNewPassword = password =>
