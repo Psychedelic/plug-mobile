584e4fe55423ef44f4f553dd832edec0592ff189
nachosan <ignacio.santise@gmail.com>
2022-05-19 14:40:55 -0300

    Solved crash in android release versions

--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -3,24 +3,24 @@ import { NativeModules } from 'react-native';
 import Reactotron, { overlay } from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
-let scriptHostname;
 if (__DEV__) {
+  let scriptHostname;
   const scriptURL = NativeModules.SourceCode.scriptURL;
   scriptHostname = scriptURL.split('://')[1].split(':')[0];
+
+  Reactotron.configure({ name: 'plugmobile', host: scriptHostname })
+    .setAsyncStorageHandler(AsyncStorage)
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(overlay())
+    .connect();
+
+  console.tron = {
+    log: Reactotron.logImportant,
+    clear: Reactotron.clear,
+    customCommand: Reactotron.onCustomCommand,
+    display: Reactotron.display,
+  };
 }
-
-Reactotron.configure({ name: 'plugmobile', host: scriptHostname })
-  .setAsyncStorageHandler(AsyncStorage)
-  .useReactNative()
-  .use(reactotronRedux())
-  .use(overlay())
-  .connect();
-
-console.tron = {
-  log: Reactotron.logImportant,
-  clear: Reactotron.clear,
-  customCommand: Reactotron.onCustomCommand,
-  display: Reactotron.display,
-};
 
 export default Reactotron;
