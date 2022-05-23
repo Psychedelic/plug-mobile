54f7eec70bf4f78ff4faf443157b9c84741445da
Matias Grote <mattgrote@Matiass-MBP.fibertel.com.ar>
2022-05-23 17:06:54 -0300

    Bridge connection working fine, method returning undefined

--- rust/.cargo/mobile_app/copy_android.sh
+++ rust/.cargo/mobile_app/copy_android.sh
@@ -5,6 +5,6 @@ mkdir -p ../../../android/app/src/main/jniLibs/x86
 mkdir -p ../../../android/app/src/main/jniLibs/arm64-v8a
 mkdir -p ../../../android/app/src/main/jniLibs/armeabi-v7a
 
-cp ./target/i686-linux-android/release/libmobile_app.so ../../../android/app/src/main/jniLibs/x86/libmobile_app.so
-cp ./target/aarch64-linux-android/release/libmobile_app.so ../../../android/app/src/main/jniLibs/arm64-v8a/libmobile_app.so
-cp ./target/armv7-linux-androideabi/release/libmobile_app.so ../../../android/app/src/main/jniLibs/armeabi-v7a/libmobile_app.so
+cp ./target/i686-linux-android/release/libic_agent_lib.so ../../../android/app/src/main/jniLibs/x86/libmobile_app.so
+cp ./target/aarch64-linux-android/release/libic_agent_lib.so ../../../android/app/src/main/jniLibs/arm64-v8a/libmobile_app.so
+cp ./target/armv7-linux-androideabi/release/libic_agent_lib.so ../../../android/app/src/main/jniLibs/armeabi-v7a/libmobile_app.so
