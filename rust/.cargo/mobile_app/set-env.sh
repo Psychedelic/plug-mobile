256f00d6e4d77ecf6af5d0e5ef41dca39c91497f
nachosan <ignacio.santise@gmail.com>
2022-06-01 15:08:12 -0300

    Improved set-env script

--- rust/.cargo/mobile_app/set-env.sh
+++ rust/.cargo/mobile_app/set-env.sh
@@ -5,27 +5,18 @@ PROJ_DIR=$(builtin cd ..; builtin cd .. ; builtin cd .. ; pwd)
 # Uncomment this line to enable full rust logs
 # export RUST_BACKTRACE=full
 
-if [ $@ = 'aarch64-apple-ios' ]; then
-  export TARGET_CC=
-elif [ $@ = 'i386-apple-ios' ]; then
-  export TARGET_CC=
-elif [ $@ = 'aarch64-linux-android' ]; then
+if [[ $@ == *android* ]]; then
   export PATH="$PROJ_DIR/NDK/arm64/bin:$PATH"
   export PATH="$PROJ_DIR/NDK/arm/bin:$PATH"
   export PATH="$PROJ_DIR/NDK/i686/bin:$PATH"
+fi
+
+if [ $@ = 'aarch64-linux-android' ]; then
   export TARGET_CC=$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android29-clang
 elif [ $@ = 'armv7-linux-androideabi' ]; then
-  export PATH="$PROJ_DIR/NDK/arm64/bin:$PATH"
-  export PATH="$PROJ_DIR/NDK/arm/bin:$PATH"
-  export PATH="$PROJ_DIR/NDK/i686/bin:$PATH"
   export TARGET_CC=$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi29-clang
 elif [ $@ = 'i686-linux-android' ]; then
-  export PATH="$PROJ_DIR/NDK/arm64/bin:$PATH"
-  export PATH="$PROJ_DIR/NDK/arm/bin:$PATH"
-  export PATH="$PROJ_DIR/NDK/i686/bin:$PATH"
   export TARGET_CC=$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android29-clang
-else
-  echo Target not detected!
 fi
 
 echo Using TARGET_CC
