1dd500a6f26ae047d8d52c1b3535bb0affafc300
nachosan <ignacio.santise@gmail.com>
2022-06-14 18:20:42 -0300

    Sending transfer amount without fixing decimals

--- src/screens/flows/Send/index.js
+++ src/screens/flows/Send/index.js
@@ -151,7 +151,7 @@ function Send({ modalRef, nft, token, onSuccess }) {
   };
 
   const handleSendToken = () => {
-    const amount = tokenAmount.value.toFixed(NUMBER_MAX_DECIMALS);
+    const amount = tokenAmount.value.toString();
     if (sendingXTCtoCanister && destination === XTC_OPTIONS.BURN) {
       dispatch(burnXtc({ to, amount }));
     } else {
