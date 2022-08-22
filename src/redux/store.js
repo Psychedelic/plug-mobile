f93b431e4e9b1ab3bc20713d3fb8113908004fed
aleacevedo <ale.acevedo@live.com.ar>
2022-08-22 10:41:07 -0300

    feat: new keyringStorageImplementation

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,15 +4,17 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import { createTransform, persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 
+import {
+  KEYRING_KEYS_IN_STORAGE,
+  KEYRING_STORAGE_KEY,
+} from '@/constants/keyring';
+import { WALLETCONNECT_STORAGE_KEY } from '@/constants/walletconnect';
+
 import Reactotron from '../config/reactotron';
 import IcpReducer from './slices/icp';
 import KeyringReducer from './slices/keyring';
 import UserReducer from './slices/user';
 import WalletConnectReducer from './slices/walletconnect';
-
-// CONSTANT
-
-export const WALLETCONNECTKEY = 'WALLETCONNECT';
 
 // PERSIST
 export const transformCircular = createTransform(
@@ -66,77 +68,63 @@ if (__DEV__ && Reactotron.setReduxStore) {
 }
 
 // KEYRING STORAGE
-const keyToLog = ['currentWalletId', 'isInitialized', 'isUnlocked'];
 
 export const keyringStorage = {
   get: async key => {
-    const state = {};
-    const isUnflatted = await AsyncStorage.getItem('unflatted');
-    if (!isUnflatted) {
-      const allKeys = await AsyncStorage.getAllKeys();
-      await Promise.all(
-        allKeys.map(async k => {
-          if (!k.includes('@REACTOTRON') && !k.includes('WALLETCONNECT')) {
-            const val = await AsyncStorage.getItem(k);
-            if (JSON.parse(val)[0]) {
-              console.log(`${k} value is undefined`);
-            }
-            const unflattedVal = JSON.parse(val)[0] || JSON.parse(`${val}`);
-            await AsyncStorage.setItem(k, JSON.stringify(unflattedVal));
-            console.log(
-              `${k} UNFLATTED`,
-              keyToLog.includes(k)
-                ? `${val} => ${JSON.stringify(unflattedVal)} SAVED: ${await AsyncStorage.getItem(k)}`
-                : 'unflattedVal'
-            );
-          }
-        })
-      );
-      await AsyncStorage.setItem('unflatted', 'true');
-    }
-    if (key) {
-      return AsyncStorage.getItem(key).then(value => JSON.parse(`${value}`));
-    } else {
-      const allKeys = await AsyncStorage.getAllKeys();
-      await Promise.all(
-        allKeys.map(async k => {
-          if (!k.includes('@REACTOTRON') && !k.includes('WALLETCONNECT')) {
-            const val = await AsyncStorage.getItem(k);
-            state[k] = JSON.parse(val);
-          }
-        })
-      );
-      return state;
-    }
-  },
-  set: async values =>
-    Promise.all(
-      Object.entries(values).map(async ([key, val]) => {
-        console.log('SETTING');
-        await AsyncStorage.setItem(key, JSON.stringify(val));
+    return AsyncStorage.getItem(KEYRING_STORAGE_KEY)
+      .then(async value => {
+        if (!value) {
+          const oldState = {};
+          await Promise.all(
+            KEYRING_KEYS_IN_STORAGE.map(async k => {
+              const flattedValue = await AsyncStorage.getItem(k);
+              oldState[k] = flattedValue
+                ? Flatted.parse(flattedValue)
+                : undefined;
+            })
+          );
+          await AsyncStorage.setItem(
+            KEYRING_STORAGE_KEY,
+            JSON.stringify(oldState)
+          );
+          return oldState;
+        }
+        return value ? JSON.parse(value) : value;
       })
-    ),
-  clear: AsyncStorage.clear,
+      .then(parsedValue => {
+        return parsedValue && key ? parsedValue[key] : parsedValue;
+      });
+  },
+  set: values => {
+    return AsyncStorage.getItem(KEYRING_STORAGE_KEY)
+      .then(savedValues => (savedValues ? JSON.parse(savedValues) : {}))
+      .then(parsedValues => {
+        return AsyncStorage.setItem(
+          KEYRING_STORAGE_KEY,
+          JSON.stringify({ ...parsedValues, ...values })
+        );
+      });
+  },
+  clear: () => AsyncStorage.removeItem(KEYRING_STORAGE_KEY),
 };
 
 export const walletConnectStorage = {
   get: async key => {
-    return AsyncStorage.getItem(WALLETCONNECTKEY)
-      .then(value => (value ? Flatted.parse(value) : value))
+    return AsyncStorage.getItem(WALLETCONNECT_STORAGE_KEY)
+      .then(value => (value ? JSON.parse(value) : value))
       .then(parsedValue => {
         return key && parsedValue ? parsedValue[key] : parsedValue;
       });
   },
   set: values => {
-    return AsyncStorage.getItem(WALLETCONNECTKEY)
-      .then(savedValues => (savedValues ? Flatted.parse(savedValues) : {}))
+    return AsyncStorage.getItem(WALLETCONNECT_STORAGE_KEY)
+      .then(savedValues => (savedValues ? JSON.parse(savedValues) : {}))
       .then(parsedValues => {
         return AsyncStorage.setItem(
-          WALLETCONNECTKEY,
-          Flatted.stringify({ ...parsedValues, ...values })
+          WALLETCONNECT_STORAGE_KEY,
+          JSON.stringify({ ...parsedValues, ...values })
         );
       });
   },
-  clear: () =>
-    AsyncStorage.setItem(WALLETCONNECTKEY, Flatted.stringify(undefined)),
+  clear: () => AsyncStorage.removeItem(WALLETCONNECT_STORAGE_KEY),
 };
