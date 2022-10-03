1cb10899142fd918b5d3e90aa69e3552f0178329
Matt Grote <matiasgrote@gmail.com>
2022-10-03 16:14:03 -0300

    Add improvements and fix problem with ICP

--- src/screens/flows/WalletConnect/screens/TimeoutError/index.tsx
+++ src/screens/flows/WalletConnect/screens/TimeoutError/index.tsx
@@ -1,4 +1,3 @@
-import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { t } from 'i18next';
 import React from 'react';
 import { Linking, View } from 'react-native';
@@ -7,32 +6,25 @@ import Button from '@/components/buttons/Button';
 import Text from '@/components/common/Text';
 import { FontStyles } from '@/constants/theme';
 import useDisableBack from '@/hooks/useDisableBack';
-import { RootStackParamList } from '@/interfaces/navigation';
+import { TNavigation } from '@/interfaces/navigation';
 import { Container } from '@/layout';
 import Routes from '@/navigation/Routes';
 
 import styles from './styles';
 
-interface Props {
-  route: {
-    params: {
-      dappUrl: string;
-      dappName: string;
-    };
-  };
-}
-
-function WCTimeoutError({ route }: Props) {
+function WCTimeoutError({
+  route,
+  navigation,
+}: TNavigation<Routes.WALLET_CONNECT_ERROR>) {
   useDisableBack();
   const { dappUrl, dappName } = route?.params || {};
-  const { reset } = useNavigation<NavigationProp<RootStackParamList>>();
 
   const gotoDapp = () => {
     Linking.openURL(dappUrl);
   };
 
   const onStartAgain = () => {
-    reset({
+    navigation.reset({
       index: 1,
       routes: [{ name: Routes.SWIPE_LAYOUT }],
     });
