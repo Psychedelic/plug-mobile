08f5373a2a79b51df21a63355616280019a48927
aleacevedo <ale.acevedo@live.com.ar>
2022-10-03 15:58:27 -0300

    add comment

--- src/constants/walletconnect.ts
+++ src/constants/walletconnect.ts
@@ -81,4 +81,4 @@ export const SIGNING_METHODS = [
   'verifyWhitelist',
 ];
 
-export const SIGNER_SERVER_URL = 'https://signer-server.0xaace.xyz';
+export const SIGNER_SERVER_URL = 'https://signer-server.0xaace.xyz'; //TODO: change to production url
