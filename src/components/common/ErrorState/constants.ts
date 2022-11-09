19554e850e52f850021a71ff058b3106bbcbc466
Matias Grote <44204622+mattgle@users.noreply.github.com>
2022-11-09 09:54:07 -0300

    TS Refactor (#532)
    * Update ReAnimated
    
    * Pass common components to TS

--- src/components/common/ErrorState/constants.ts
+++ src/components/common/ErrorState/constants.ts
@@ -2,7 +2,14 @@ import { t } from 'i18next';
 
 import { ERROR_TYPES } from '@/constants/general';
 
-export const getErrorStateData = type =>
+export interface ErrorStateData {
+  emoji: string;
+  title: string;
+  description: string;
+  buttonTitle: string;
+}
+
+export const getErrorStateData = (type: ERROR_TYPES) =>
   ({
     [ERROR_TYPES.FETCH_ERROR]: t(`errors.${ERROR_TYPES.FETCH_ERROR}`, {
       returnObjects: true,
